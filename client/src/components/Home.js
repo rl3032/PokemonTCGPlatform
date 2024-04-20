6e3394b6b075bcdc1c6dcc6ea5884389c4c58bf2
Runjie Lyu <runjielyu@gmail.com>
2024-04-20 11:31:38 -0700

    Add home related components

---
+++ client/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FeatureCards from "./FeatureCards";
+import FlashSale from "./FlashSale";
+
+function Home() {
+  return (
+    <div>
+      <FeatureCards />
+      <FlashSale />
+    </div>
+  );
+}
+
+export default Home;
